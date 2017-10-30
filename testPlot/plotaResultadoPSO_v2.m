6495e12b328ef30c27e84dbb834eec6dffb896fb
wandergibaut <wandergibaut@github.com>
2017-10-30 17:14:53 -0200

    add a possible plot func

---
+++ testPlot/plotaResultadoPSO_v2.m
@@ -0,0 +1,124 @@
+function []=plotaResultadoPSO_v2(resultado,fun,options)
+% Entradas da Função:
+%   resultado: estrutura contendo os resultados
+%           resultado.totalIt: # total de iterações
+%           resultado.pop_it: matriz contendo as posições de cada partícula
+%                             em cada uma das iterações (cada par de vetor
+%                             colunas corresponde a uma iteração)
+%           resultado.x_it: matriz contendo a melhor posição (solução) de
+%                             cada iteração (cada linha corresponde a uma iteração)
+%           resultado.x:   vetor linha de dimensão 2, corresponde a melhor
+%                             solução encontrada pelo algoritmo
+%
+%   fun: um function_handle contendo a função a ser plotada:
+%        exemplo:
+%                   fun=@(a)-((a(1)-1)^2+(a(2)-1)^2);
+%
+%  options: estrutura contendo as opções
+%           options.limites: [lmin lmax] -> limites mínimo e máximo do
+%                            gráfico
+%           
+
+
+maxIt=resultado.totalIt;
+lim=options.limites;
+
+x=linspace(lim(1),lim(2),200);
+y=linspace(lim(1),lim(2),200);
+[X,Y] = meshgrid(x,y);
+
+for i=1:200
+    for j=1:200
+        a=[x(i) y(j)];
+        F(i,j)=fun(a);
+    end
+end
+
+figura=figure('Position', [900    300    1000    400]);
+
+figura3d=subplot(1,2,1);
+superficie=surf(x,y,F,'EdgeColor','none');
+alpha(superficie,0.3)
+hold
+colormap(parula)
+% savefig(Figura,['funcao3d.fig']);
+
+
+figura2d=subplot(1,2,2);
+
+contourf(x,y,F,50,'EdgeColor','none')
+hold
+colormap(parula)
+colorbar
+axis([-10 10 -10 10])
+pbaspect([1 1 1])
+xticks([-10 -8 -6 -4 -2 0 1 2 4 6 8 10])
+yticks([-10 -8 -6 -4 -2 0 1 2 4 6 8 10])
+grid
+% savefig(Figura,['funcao2d.fig']);
+
+
+pop_it=resultado.pop_it;
+x_it=resultado.x_it;
+N=size(pop_it,2);
+
+
+ind=round(linspace(1,N-1,maxIt));
+
+x1=pop_it(:,1);
+x2=pop_it(:,2);
+
+X1=x_it(1,1); X2=x_it(1,2);
+
+subplot(figura2d)
+plot(x1,x2,'.','color',[0 0 0]);
+part2d=plot(x1,x2,'.','color',[0.8 0 0]);
+melhor2d=plot(X1,X2,'*','color',[1 0 0]);
+
+title(['Iteração: 1']);
+
+subplot(figura3d)
+for j=1:max(size(x1))
+    f(j)=fun([x1(j),x2(j)]);
+end
+F=fun([X1,X2]);
+plot3(x1,x2,f,'.','color',[0 0 0]);
+part3d=plot3(x1,x2,f,'.','color',[0.8 0 0]);
+melhor3d=plot3(X1,X2,F,'*','color',[1 0 0]);
+title(['Iteração: 1']);
+
+pause(0.1)    
+for i=2:size(ind,2)
+
+    x1=pop_it(:,ind(i));
+    x2=pop_it(:,ind(i)+1);
+
+    X1=x_it(i,1); X2=x_it(i,2);
+
+    for j=1:max(size(x1))
+        f(j)=fun([x1(j),x2(j)]);
+    end
+
+    F=fun([X1,X2]);
+
+    set(part2d,'XData',x1,'YData',x2);
+    set(melhor2d,'XData',X1,'YData',X2);
+    set(part3d,'XData',x1,'YData',x2,'ZData',f);
+    set(melhor3d,'XData',X1,'YData',X2,'ZData',F);
+    subplot(figura2d)
+    title(['Iteração: ',num2str(i)]);
+    subplot(figura3d)
+    title(['Iteração: ',num2str(i)]);
+    pause(0.1)
+
+
+end
+
+subplot(figura2d)
+x_sol=resultado.x;
+plot(x_sol(1),x_sol(2),'r*')
+
+subplot(figura3d)
+plot3(x_sol(1),x_sol(2),fun([x_sol(1),x_sol(2)]),'r*')
+    
+
